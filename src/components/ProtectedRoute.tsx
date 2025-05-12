ccc8e04f40c99c0cf75f34985e3aff9b1615b07b
Serhat YILDIZ <132045435+Serhat1881@users.noreply.github.com>
2025-05-13 00:19:30 +0300

    Add authentication routing protection

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,10 @@
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default ProtectedRoute; 
